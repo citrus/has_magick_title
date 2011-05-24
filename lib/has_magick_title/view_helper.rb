673ea954463b83253c8ee3c3fde540a80fa4d53f
Spencer Steffen <spencer@citrusme.com>
2011-05-24 15:17:59 -0700

    use magick_title_text rather than options[:attribute]

--- lib/has_magick_title/view_helper.rb
+++ lib/has_magick_title/view_helper.rb
@@ -1,13 +1,13 @@
 module HasMagickTitle
-
   module ViewHelper
   
+    # Creates and HTML image tag with the options provided
     def magick_title_for(record, options={})
+      opts = record.magick_title_options[:to_html].merge(options)
       record.refresh_magick_title unless record.has_image_title? 
       return "[magick_title error]" unless record.has_image_title?
-      image_tag record.image_title.url, :alt => record.magick_title_text, :title => record.magick_title_text, :class => record.magick_title_text
-    end
+      MagickTitle::Renderer.to_html(record.magick_title_text, record.image_title.url, opts).html_safe
+    end        
   
   end
-     
 end
